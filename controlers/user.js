0ca3a12a1cdd01e88ff80c303af1b2858780196f
VadimGlumakoff <glumakoffv@yandex.ru>
2023-10-22 17:04:42 +0300

    diplom(попытка2)

--- controlers/user.js
+++ controlers/user.js
@@ -76,6 +76,7 @@ const login = async (req, res, next) => {
             httpOnly: true,
             maxAge: 24 * 60 * 60 * 1000,
             sameSite: "None",
+            secure: true,
         };
         res.cookie("jwtToken", token, cookieOption);
         res.send({ message: "Успешно вошли" });
