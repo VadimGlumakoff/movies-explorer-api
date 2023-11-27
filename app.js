97ee83fe2f4448a189d9c7c63d0de5b840991ac7
VadimGlumakoff <glumakoffv@yandex.ru>
2023-11-27 21:45:47 +0300

    434

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const router = require("./routes/index");
 
 const { PORT = 3000, DB_URL, NODE_ENV } = process.env;
 
-mongoose.connect(NODE_ENV === "production" ? DB_URL : "mongodb://localhost:27017/bitfilmsdb", {
+mongoose.connect(NODE_ENV === "production" ? DB_URL : "mongodb://127.0.0.1:27017/bitfilmsdb", {
     family: 4,
     useNewUrlParser: true,
     useUnifiedTopology: true,
