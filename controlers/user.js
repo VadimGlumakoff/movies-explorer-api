616372adea2b624c5b7c2050f888da3e37407fec
VadimGlumakoff <glumakoffv@yandex.ru>
2023-12-01 20:57:19 +0300

    3

--- controlers/user.js
+++ controlers/user.js
@@ -75,7 +75,7 @@ const login = async (req, res, next) => {
         const cookieOption = {
             httpOnly: true,
             maxAge: 24 * 60 * 60 * 1000,
-            sameSite: "None",
+            sameSite: "none",
             secure: true,
         };
         res.cookie("jwt", token, cookieOption);
